3c917d308bc36733dc2e440c2190146c267516b5
Andreas Lutz <andreasl.95@web.de>
2025-02-15 14:00:18 +0100

    Added missing math import in utils

--- ood_detection/src/helper/utils.py
+++ ood_detection/src/helper/utils.py
@@ -1,3 +1,5 @@
+import math
+
 def compute_conv_output_size(size: int, kernel_size: int, stride: int, padding: int, dilation: int = 1) -> int:
     """
     Computes the output size of a convolutional layer along one dimension.
